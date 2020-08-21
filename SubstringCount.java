95c24024c3fbd751f39ff3b66eafddcf720cce7c
vivek-1999-hpvi <54849743+vivek-1999-hpvi@users.noreply.github.com>
2020-08-21 21:52:32 +0530

    Add files via upload

---
+++ SubstringCount.java
@@ -0,0 +1,57 @@
+package Assignment1;
+import java.util.Scanner;
+public class SubstringCount {    
+    private boolean isCountZero(int[] chars){
+        for(int i: chars)
+            if(i != 0)
+                return false;
+        
+        return true;
+    }
+    
+    public int substringCount(String str, String subStr){
+        if(str == null || str.length() < 1 || subStr == null || subStr.length() < 1)
+            return 0;
+        
+        if(str.length() < subStr.length())
+            return 0;
+        
+        int[] strWinCC = new int[26];
+        int winSize = subStr.length(), count = 0;
+   
+        for(char c: subStr.toCharArray())
+            strWinCC[c - 'a']++;
+        for(char c: str.substring(0, winSize).toCharArray())
+            strWinCC[c - 'a']--;
+        
+        count += (isCountZero(strWinCC))? 1: 0;
+        
+        for(int i=winSize; i<str.length(); ++i){
+            
+            strWinCC[str.charAt(i) - 'a']--;
+            
+            strWinCC[str.charAt(i-winSize) - 'a']++;
+            
+            count += (isCountZero(strWinCC))? 1: 0;
+        }
+        
+        return count;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Program to count the sub-string in a string if the order of characters doesn't matter\n");
+        System.out.println("Enter string(only lowercase alphabets[a-z]):");
+        String str = sc.nextLine();
+        System.out.println("Enter sub-string:");
+        String substr = sc.nextLine();
+        
+        System.out.print("Count: ");
+        System.out.println((new SubstringCount()).substringCount(str.toLowerCase(), substr.toLowerCase()));
+        
+    }
+    
+}
