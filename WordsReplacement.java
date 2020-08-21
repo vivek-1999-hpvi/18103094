18389e5665b6289f2737e6e51f11c8890931ce18
vivek-1999-hpvi <54849743+vivek-1999-hpvi@users.noreply.github.com>
2020-08-21 23:54:21 +0530

    Update WordsReplacement.java

--- WordsReplacement.java
+++ WordsReplacement.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ques2{
+public class WordsReplacement{
 	public static void main(String[] args) {
 		System.out.println("Enter the string:");
 		Scanner scan=new Scanner(System.in);
