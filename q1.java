5c7faf4ae702f7007d135da5fb5f44f718fd6a20
vivek-1999-hpvi <54849743+vivek-1999-hpvi@users.noreply.github.com>
2020-10-12 19:01:06 +0530

    Add files via upload

--- q1.java
+++ q1.java
@@ -1,74 +1,188 @@
 import java.util.Scanner;
-public class q1{
-    public static void main(String args[]){
-        Scanner obj = new Scanner(System.in);
-        System.out.print("Enter two strings (line-seperated): ");
-        String s1, s2;
-        s1 = obj.nextLine();
-        s2 = obj.nextLine();
-
-        boolean equal = true;
-        int n1 = s1.length(),n2 = s2.length(), n;
-        if(n1<n2){
-            n = n1;
+import java.util.*;
+import java.lang.Math.*;
+class q1
+{
+    public static void main(String[] args)
+    {
+        int n, m;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+        in.nextLine();
+        String temp;
+        String[] crops = new String[n];
+        for (int i=0;i<n;i++)
+        {
+            crops[i]=in.nextLine().trim();
         }
-        else{
-            n = n2;
-        }
-
-        int i = 0;
-        while(i<n){
-
-            int one = s1.charAt(i), two = s2.charAt(i);
-             if(((65<=one && one<=90) || (97<=one && one<=120)) && ((65<=two && two<=90) || (97<=two && two<=120))){
-                 if(one-two==32 || two-one==32 || one==two){
-                    i+=1;
-                    continue;
-                 }
-                 else if(one-two<32 || two-one>32){
-                     equal = false;
-                     System.out.println("Smaller string: "+s1);
-                     System.out.println("Larger string: "+s2);
-                     break;
-                 }
-                 else{
-                 equal = false;
-                 System.out.println("Smaller string: "+s2);
-                 System.out.println("Larger string: "+s1);
-                 break;
-                 }
-             }
-
-             else
-            if(one<two){
-                equal = false;
-                System.out.println("Smaller string: "+s1);
-                System.out.println("Larger string: "+s2);
-                break;
-            }
-
-            else if(two<one){
-                equal = false;
-                System.out.println("Smaller string: "+s2);
-                System.out.println("Larger string: "+s1);
-                break;
-            }
-            i+=1;
-        }
-
-        if(equal){
-            if(n1>n){
-                System.out.println("Smaller string: "+s2);
-                System.out.println("Larger string: "+s1);
-            }
-            else if(n2>n){
-                System.out.println("Smaller string: "+s1);
-                System.out.println("Larger string: "+s2);
-            }
-            else{
-                System.out.println(s1 +" and "+ s2 +" are equal");
+        char[][] crops_mat = new char[n][m];
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0;j<m; j++)
+            {
+                crops_mat[i][j] = crops[i].charAt(j);
             }
         }
+        char[] crop_types = new char[26];
+        for(char c='a'; c<'z'; c++)
+        {
+            crop_types[c-'a'] = c;
+        }
 
+        System.out.println(replant(crops_mat, 0, 0, n, m, 0, n*m, crop_types));
+    }
+
+    public static int replant(char[][] crops_mat, int curr_row, int curr_col, int n, int m, int curr_count, int minm_count, char[] crop_types)
+    {
+        Set<Character> left_up = new HashSet<Character>();
+        if(curr_row!=0)
+        {
+            left_up.add(crops_mat[curr_row-1][curr_col]);
+        }
+        if(curr_col!=0)
+        {
+            left_up.add(crops_mat[curr_row][curr_col-1]);
+        }
+        Set<Character> right_down = new HashSet<Character>();
+        if(curr_row!=n-1)
+        {
+            right_down.add(crops_mat[curr_row+1][curr_col]);
+        }
+        if(curr_col!=m-1)
+        {
+            right_down.add(crops_mat[curr_row][curr_col+1]);
+        }
+        boolean present_left_up = false;
+        if(left_up.contains(crops_mat[curr_row][curr_col]))
+        {
+            present_left_up = true;
+        }
+        char[][] new_crops_mat = new char[n][m];
+        if(right_down.contains(crops_mat[curr_row][curr_col]))
+        {
+            if(present_left_up)
+            {
+                for(int k=0; k<26; k++)
+                {
+                    if(!left_up.contains(crop_types[k]) && !right_down.contains(crop_types[k]))
+                    {
+                        for(int i=0; i<n; i++)
+                        {
+                            for(int j=0;j<m;j++)
+                            {
+                                new_crops_mat[i][j] = crops_mat[i][j];
+                            }
+                        }
+                        new_crops_mat[curr_row][curr_col] = crop_types[k];
+                        if(curr_col<m-1)
+                        {
+                            minm_count = Math.min(minm_count, replant(new_crops_mat, curr_row, curr_col+1, n, m, curr_count+1, minm_count, crop_types));
+                        }
+                        else if(curr_row<n-1)
+                        {
+                            minm_count = Math.min(minm_count, replant(new_crops_mat, curr_row+1, 0, n, m, curr_count+1, minm_count, crop_types));
+                        }
+                        else
+                        {
+                            return Math.min(curr_count+1, minm_count);
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                if(curr_col<m-1)
+                {
+                    minm_count = Math.min(minm_count, replant(crops_mat, curr_row, curr_col+1, n, m, curr_count, minm_count, crop_types));
+                }
+                else if(curr_row<n-1)
+                {
+                    minm_count = Math.min(minm_count, replant(crops_mat, curr_row+1, 0, n, m, curr_count, minm_count, crop_types));
+                }
+                else
+                {
+                    return Math.min(curr_count, minm_count);
+                }
+                for(int k=0; k<26;k++)
+                {
+                    if(!left_up.contains(crop_types[k]) && !right_down.contains(crop_types[k]))
+                    {
+                        for(int i=0; i<n; i++)
+                        {
+                            for(int j=0; j<m; j++)
+                            {
+                                new_crops_mat[i][j] = crops_mat[i][j];
+                            }
+                        }
+                        new_crops_mat[curr_row][curr_col] = crop_types[k];
+                        if(curr_col<m-1)
+                        {
+                            minm_count = Math.min(minm_count, replant(new_crops_mat, curr_row, curr_col+1, n, m, curr_count+1, minm_count, crop_types));
+                        }
+                        else if(curr_row<n-1)
+                        {
+                            minm_count = Math.min(minm_count, replant(new_crops_mat, curr_row+1, 0, n, m, curr_count+1, minm_count, crop_types));
+                        }
+                        else
+                        {
+                            return Math.min(curr_count+1, minm_count);
+                        }
+                        break;
+                    }
+                }
+
+            }
+        }
+        else
+        {
+            if(present_left_up)
+            {
+                for(int k=0; k<26;k++)
+                {
+                    if(!left_up.contains(crop_types[k]) && !right_down.contains(crop_types[k]))
+                    {
+                        for(int i=0; i<n; i++)
+                        {
+                            for(int j=0;j<m;j++)
+                            {
+                                new_crops_mat[i][j] = crops_mat[i][j];
+                            }
+                        }
+                        new_crops_mat[curr_row][curr_col] = crop_types[k];
+                        if(curr_col<m-1)
+                        {
+                            minm_count = Math.min(minm_count, replant(new_crops_mat, curr_row, curr_col+1, n, m, curr_count+1, minm_count, crop_types));
+                        }
+                        else if(curr_row<n-1)
+                        {
+                            minm_count = Math.min(minm_count, replant(new_crops_mat, curr_row+1, 0, n, m, curr_count+1, minm_count, crop_types));
+                        }
+                        else
+                        {
+                            return Math.min(curr_count+1, minm_count);
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                if(curr_col<m-1)
+                {
+                    minm_count = Math.min(minm_count, replant(crops_mat, curr_row, curr_col+1, n, m, curr_count, minm_count, crop_types));
+                }
+                else if(curr_row<n-1)
+                {
+                    minm_count = Math.min(minm_count, replant(crops_mat, curr_row+1, 0, n, m, curr_count, minm_count, crop_types));
+                }
+                else
+                {
+                    return Math.min(curr_count, minm_count);
+                }
+            }
+        }
+        return minm_count;
     }
 }
--- q2.java
+++ q2.java
@@ -1,24 +1,15 @@
-import java.util.Arrays;
-public class q2 {
-    
-    public void sort(int[] arr){
-        int[] count = new int[21];
-        
-        for(int i: arr)
-            count[i]++;
-        
-        int lo = 0;
-        for(int i=0; i<21; ++i){
-            int temp = lo;
-            while(lo < temp+count[i])
-                arr[lo++] = i;
-        }
-        
+class q1
+{
+    public static void main(String[] args)
+    {
+        int num = -1;
+        byte b = (byte)num;
+        int n = b;
+        char c = (char)b, c1 = (char)n;
+        int final_num = c;
+        System.out.println("Initial integer: "+ num); // -1
+        System.out.println("Byte: "+ b + " " + n + " " + c1); // -1
+        System.out.println("Character: "+ c); // a rectangle char
+        System.out.println("Final integer: "+ final_num); // 65535
     }
-    public static void main(String[] args) {
-        int[] arr = {4,2,5,6,1,2,9,0,2,10};
-        (new q2()).sort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-    
 }
